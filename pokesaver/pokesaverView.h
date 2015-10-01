282cdc96a09b0294b46fdc5653bbefdcf7879351
Flavien Raynaud <flavien.raynaud@gmail.com>
2015-10-01 11:34:42 +0200

    Initial Commit

---
+++ pokesaver/pokesaverView.h
@@ -0,0 +1,13 @@
+//
+//  pokesaverView.h
+//  pokesaver
+//
+//  Created by Flavien Raynaud on 01/10/15.
+//  Copyright © 2015 Flavien Raynaud. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface pokesaverView : ScreenSaverView
+
+@end
---
+++ pokesaver/pokesaverView.m
@@ -0,0 +1,52 @@
+//
+//  pokesaverView.m
+//  pokesaver
+//
+//  Created by Flavien Raynaud on 01/10/15.
+//  Copyright © 2015 Flavien Raynaud. All rights reserved.
+//
+
+#import "pokesaverView.h"
+
+@implementation pokesaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
